4087d02d61e65af12eae443a036b1b0af3f249aa
rocky <rocky55737@gmail.com>
2022-12-17 01:25:23 +0900

    should be leak check

--- philo/main.c
+++ philo/main.c
@@ -6,14 +6,20 @@
 /*   By: rocky <rocky@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 17:02:10 by rhong             #+#    #+#             */
-/*   Updated: 2022/12/16 23:37:33 by rocky            ###   ########.fr       */
+/*   Updated: 2022/12/17 01:25:05 by rocky            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./philo.h"
 
+void leaks(void)
+{
+	system("leaks philo\n");
+}
+
 int	main(int ac, char **av)
 {
+	atexit(leaks);
 	if (ph_input_err_handler(ac, av))
 	{
 		write(2, "input err\n", 10);
